542af1bf43de56e7d42ea50ee8726ac24332f157
hp <vkhomutova2005@gmail.com>
2023-04-09 20:05:34 +0300

    main_page/collars

---
+++ data/collars_resource.py
@@ -0,0 +1,55 @@
+from flask import abort
+
+from flask import jsonify
+
+from .simple_tables import Collars
+from . import db_session
+
+from flask_restful import abort, Resource, reqparse
+parser = reqparse.RequestParser()
+parser.add_argument('name', required=True, type=str)
+parser.add_argument('picture', required=True)
+parser.add_argument('deleted', required=True, type=bool)
+
+
+def abort_if_collars_not_found(collars_id):
+    session = db_session.create_session()
+    collars = session.query(Collars).get(collars_id)
+    if not collars:
+        abort(404, message=f"Collar {collars_id} not found")
+
+
+class CollarsResource(Resource):
+    def get(self, collars_id):
+        abort_if_collars_not_found(collars_id)
+        session = db_session.create_session()
+        collars = session.query(Collars).get(collars_id)
+        return jsonify({'collar': collars.to_dict(
+            only=('name', 'picture', 'deleted'))})
+
+    def delete(self, collars_id):
+        abort_if_collars_not_found(collars_id)
+        session = db_session.create_session()
+        collars = session.query(Collars).get(collars_id)
+        session.delete(collars)
+        session.commit()
+        return jsonify({'success': 'OK'})
+
+
+class CollarsListResource(Resource):
+    def get(self):
+        session = db_session.create_session()
+        collars = session.query(Collars).all()
+        return jsonify({'collars': [item.to_dict(
+            only=('id', 'name', 'picture', 'deleted')) for item in collars]})
+
+    def post(self):
+        args = parser.parse_args()
+        session = db_session.create_session()
+        collars = Collars(
+            name=args['name'],
+            picture=args['picture'],
+            deleted=args['deleted'])
+        session.add(collars)
+        session.commit()
+        return jsonify({'success': 'OK'})
--- main.py
+++ main.py
@@ -1,16 +1,54 @@
-# This is a sample Python script.
+from flask import Flask, render_template, redirect, request, abort, make_response
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from flask import jsonify
+from flask_restful import abort, Api
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# from forms.jobs import JobsForm
+# from forms.user import RegisterForm, LoginForm
+from data.user import User
+from data.simple_tables import Collars
+
+from data import db_session
+from data import collars_resource
+
+app = Flask(__name__)
+api = Api(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    db_session.global_init("db/designer_base.db")
+    # app.register_blueprint(users_api.blueprint)
+    # app.register_blueprint(jobs_api.blueprint)
+    # для списка объектов
+    api.add_resource(collars_resource.CollarsListResource, '/api/v2/collars')
+    # для одного объекта
+    api.add_resource(collars_resource.CollarsResource, '/api/v2/collars/<int:collars_id>')
+    app.run()
 
 
-# Press the green button in the gutter to run the script.
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.errorhandler(400)
+def bad_request(_):
+    return make_response(jsonify({'error': 'Bad Request'}), 400)
+
+
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    # if current_user.is_authenticated:
+    #     jobs = db_sess.query(Jobs).filter((Jobs.user == current_user) | (Jobs.is_finished != True))
+    # else:
+    #     jobs = db_sess.query(Jobs).filter(Jobs.is_finished != True)
+    return render_template("index.html")
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
