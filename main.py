90595b0379acd301cb33002dbe8a897d38d8a09e
skgch-heigo <mstal64@yandex.ru>
2023-04-10 21:01:19 +0400

    added registration and login

--- main.py
+++ main.py
@@ -22,6 +22,9 @@ from data import collars_resource
 
 from data.constants.tables_inf import TABLES
 
+from data.forms.login_in import LoginInForm
+from data.forms.registration_form import RegisterForm
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -83,6 +86,48 @@ def element_information(table, id_):
     if table == "users":
         pass
     return redirect("/")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginInForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        # print(form.password.data)
+        # если вам скучно, то разкомментируйте эту^ строку
+        # запустите сервер и отправьте ссылку в классный чат с просьбой потестить
+        # 100% кто-нибудь зарегистрируется с настоящими почтой и паролем (уже такое было)
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login_in.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login_in.html', title='Авторизация', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reg():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        ans = {"name": "", "email": "", "password": ""}
+        for i in ans:
+            if i in request.form:
+                ans[i] = request.form[i]
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('used_email.html')  # если пытаться зарегистрироваться с почтой, которая уже есть
+        user = User()
+        user.name = ans["name"]
+        user.email = ans["email"]
+        user.hashed_password = generate_password_hash(ans["password"])
+        # print([ans["password"]], user.hashed_password, generate_password_hash(ans["password"]))
+        db_sess.add(user)
+        db_sess.commit()
+        login_user(user)
+        return redirect("/")
+    return render_template('user_form.html', title='Регистрация', form=form)
 
 
 @app.errorhandler(400)
