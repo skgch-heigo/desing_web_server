99f7dc877a9da7ad7d0f312634b7d7071bc83e4a
hp <vkhomutova2005@gmail.com>
2023-04-11 15:22:09 +0300

    collar_resourse_2

--- data/resources/collars_resource.py
+++ data/resources/collars_resource.py
@@ -9,7 +9,6 @@ from flask_restful import abort, Resource, reqparse
 parser = reqparse.RequestParser()
 parser.add_argument('name', required=True, type=str)
 parser.add_argument('picture', required=True)
-parser.add_argument('deleted', required=True, type=bool)
 
 
 def abort_if_collars_not_found(collars_id):
@@ -23,9 +22,12 @@ class CollarsResource(Resource):
     def get(self, collars_id):
         abort_if_collars_not_found(collars_id)
         session = db_session.create_session()
-        collars = session.query(Collars).get(collars_id)
-        return jsonify({'collar': collars.to_dict(
-            only=('name', 'picture', 'deleted'))})
+        for _ in session.query(Collars).filter(Collars.deleted == 1, Collars.id == collars_id):
+            return jsonify({f'collar {collars_id}': 'deleted'})
+        else:
+            collars = session.query(Collars).get(collars_id)
+            return jsonify({'collar': collars.to_dict(
+                only=('name', 'picture'))})
 
     def delete(self, collars_id):
         abort_if_collars_not_found(collars_id)
@@ -39,17 +41,17 @@ class CollarsResource(Resource):
 class CollarsListResource(Resource):
     def get(self):
         session = db_session.create_session()
-        collars = session.query(Collars).all()
-        return jsonify({'collars': [item.to_dict(
-            only=('id', 'name', 'picture', 'deleted')) for item in collars]})
+        collars = session.query(Collars).filter(Collars.deleted == 0)
+        if collars:
+            return jsonify({'collars': [item.to_dict(
+                only=('id', 'name', 'picture')) for item in collars]})
 
     def post(self):
         args = parser.parse_args()
         session = db_session.create_session()
         collars = Collars(
             name=args['name'],
-            picture=args['picture'],
-            deleted=args['deleted'])
+            picture=args['picture'])
         session.add(collars)
         session.commit()
         return jsonify({'success': 'OK'})
